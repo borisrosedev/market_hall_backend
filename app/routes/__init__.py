7b91b239eb2f1c90de55dbf9acc84e4e7c0c939d
borisrosedev <boris.rose.dev@gmail.com>
2025-08-08 11:44:12 +0200

    feat/products

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,2 +1,3 @@
 from .users import api_v1_users
 from .auth import api_v1_auth
+from .products import api_v1_products
---
+++ app/routes/products.py
@@ -0,0 +1,21 @@
+import logging
+
+from flask import Blueprint, request, jsonify, session
+from ..database import db 
+from ..services import admin_required_with_exceptions
+from ..services import json_required_with_validation, session_required
+api_v1_products = Blueprint("api_v1_products", __name__,url_prefix="/api/v1/products")
+
+logging.basicConfig(level=logging.DEBUG)
+
+# api_v1_products.before_request() ( check to see if you put the decorator in before requrest )
+
+# route to handle the create of a new product 
+
+# route to handle the getting of all products
+
+# route to handler the getting of one product
+
+# route to  handle the update of a product 
+
+# route to handle the deletion of a product 
