f032f5400e514ba8566e3fb2a4856abb616b0821
GuillotBoris <guillotboris@hotmail.com>
2025-08-12 11:48:15 +0200

    feature/curl-test-product-botticielli

--- api.sh
+++ api.sh
@@ -33,7 +33,7 @@ function test_update_product(){
     -d '{"name":"test","description":"test","price":100,"quantity":10}'
 }
 
-function test_create_product(){
+function test_create_product_daVinci(){
     curl -X POST http://localhost:5000/api/v1/products/ \
     -H "Content-Type: multipart/form-data" \
     -F "name=Mona Lisa" \
@@ -56,10 +56,23 @@ function test_create_product_vermeer(){
     -F "file=@./la-jeune-fille-a-perle.jpg"
 }
 
+function test_create_product_botticelli(){
+    curl -X POST http://localhost:5000/api/v1/products/ \
+    -H "Content-Type: multipart/form-data" \
+    -F "name=La naissance de Vénus" \
+    -F "description=La Naissance de Vénus est un tableau de Sandro Botticelli, peint vers 1482-1485 et conservé à la galerie des Offices. Il a été peint selon la technique de la tempera. Il représente la déesse Vénus arrivant sur le rivage après sa naissance. Artist  Sandro Botticelli" \
+    -F "price=5240000" \
+    -F "tags=painting,art" \
+    -F "quantity=1" \
+    -F "file=@./la_nascita_di_Venere.gif"
+}
+
 #test_post
 #test_get_all_product 
 # test_get_ById_product
 # test_update_product
 
-#test_create_product
+test_create_product_daVinci
 test_create_product_vermeer
+test_create_product_botticelli
+
